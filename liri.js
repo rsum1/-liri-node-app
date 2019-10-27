1804b28dce826c5e7e29f54f26de3d25f518fe97
rsum1 <ryanjsumpter+github@gmail.com>
2019-10-27 14:26:42 -0700

    initialize npm's, set up switch cases

--- liri.js
+++ liri.js
@@ -2,4 +2,28 @@ require("dotenv").config()
 
 var keys = require("./keys.js")
 
+var Spotify = require('node-spotify-api')
 var spotify = new Spotify(keys.spotify)
+
+var axios = require('axios')
+
+let command = process.argv[2]
+let query = process.argv[3]
+
+switch (command) {
+  case 'concert-this':
+    findConcert(query)
+    break;
+  case 'spotify-this-song':
+    songInfo(query)
+    break;
+  case 'movie-this':
+    movieInfo(query)
+    break;
+  // case 'do-what-it-says':
+
+  //   break;
+  default:
+    console.log('error')
+    break
+}
